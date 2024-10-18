1aa722ab259157842c1043498aacd37930840b8f
Ari Thompson <arianathompson@my.capilanou.ca>
2024-10-17 17:15:04 -0700

    agony

---
+++ js/animate.js
@@ -0,0 +1,31 @@
+$(function () {
+  $(".p2").typed({
+    strings: ["DESIGNER", "DEVELOPER", "CREATIVE"],
+    typeSpeed: 50,
+    backSpeed: 10,
+    backDelay: 2000,
+    showCursor: false,
+    loop: false,
+  });
+});
+
+$(".hero-down").mousedown(function () {
+  TweenMax.fromTo(
+    ".btn-react",
+    0.25,
+    {
+      opacity: 0,
+      scale: 0,
+    },
+    {
+      opacity: 0.25,
+      scale: 1,
+      onComplete: function () {
+        TweenMax.to(".btn-react", 0.25, {
+          opacity: 0,
+          scale: 0,
+        });
+      },
+    }
+  );
+});
